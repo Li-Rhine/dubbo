3f554284673937f350b0d0d93903cce4d2250f3f
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2019-08-29 01:24:28 +0800

    controller代码

--- dubbo/user-web/src/main/java/com/xdclass/user/controller/UserController.java
+++ dubbo/user-web/src/main/java/com/xdclass/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.xdclass.user.controller;
 
 import com.xdclass.user.service.UserService;
 import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
@@ -11,6 +13,6 @@ public class UserController {
 
     @RequestMapping("/sayHello")
     public String sayHello(){
-
+        return userService.sayHello();
     }
 }
