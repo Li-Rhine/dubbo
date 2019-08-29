fbf613f541d0024c65632022af1677048e8f2749
dahai.li <dahai.li@china.zhaogang.com>
2019-08-29 13:51:47 +0800

    yml配置文件

---
+++ dubbo/user-web/src/main/java/com/xdclass/user/WebApplication.java
@@ -0,0 +1,16 @@
+package com.xdclass.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: dahai.li
+ * @Description:
+ * @Date: Create in 13:44 2019/8/29
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class);
+    }
+}
